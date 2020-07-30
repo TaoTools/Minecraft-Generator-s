166ae05cd88fe960ce4b59f1b4310ee62540ced7
taonix314 <taonix314@gmail.com>
2020-07-31 01:03:48 +0200

    cookies

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,46 @@
+function setCookie(name, value, expirationInDay){
+    let date = new Date();
+    date.setTime(date.getTime()+(expirationInDay*24*60*60*1000));
+    let expires = "expires="+ date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path/";
+}
+
+function getCookie(name1) {
+    let name = name1 + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' '){
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0){
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkThemeCookie(){
+    let siteTheme = getCookie('siteTheme');
+    if(siteTheme === 'sombre'){
+        $("body").css("background-color", "#292b2f");
+        $(".callout-primary").removeClass("callout-primary").addClass("callout-dark");
+        $('button, a').css("background-color", "#2f3136").css("color", "#2e3032").removeClass("btn-primary").addClass("btn-secondary");
+        $('.card').css("background-color", "#36393f");
+        $('.callout').css("background-color", "#36393f");
+        $('h5, label, p, label, div, button, .badge, a').css("color", "#e2e2e2");
+        $( "#theme" ).prop( "checked", true );
+    }else{
+        console.log('Pas de cookie siteTheme');
+        setCookie('siteTheme', 'clair', 0.007);
+    }
+}
+
+checkThemeCookie();
+
+
+
 $('#list').html(
     $('#list li')
         .get()
@@ -59,8 +102,11 @@ $(window).on("load", function () {
             $('.card').css("background-color", "#36393f");
             $('.callout').css("background-color", "#36393f");
             $('h5, label, p, label, div, button, .badge, a').css("color", "#e2e2e2");
+            setCookie('siteTheme', 'sombre', 0.007);
+
         }
         else {
+            setCookie('siteTheme', 'clair', 0.007);
             document.location.reload(true);
         }
     });
