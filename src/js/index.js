3fa86db6b67a243e8adb1b8fe71ae9db2af4989a
taonix314 <taonix314@gmail.com>
2020-07-30 23:54:52 +0200

    Dark Theme colors updated

--- src/js/index.js
+++ src/js/index.js
@@ -53,12 +53,12 @@ $(window).on("load", function () {
     $('#theme').on('click', function () {
         if ($('#theme').is(':checked')) {
 
-            $("body").css("background-color", "#3c3f41");
+            $("body").css("background-color", "#292b2f");
             $(".callout-primary").removeClass("callout-primary").addClass("callout-dark");
-            $('button, a').css("background-color", "#d6d5d5").css("color", "#2e3032").removeClass("btn-primary").addClass("btn-secondary");
-            $('.card').css("background-color", "#838383");
-            $('.callout').css("background-color", "#838383");
-            $('h5, label, p, label').css("color", "#e2e2e2");
+            $('button, a').css("background-color", "#2f3136").css("color", "#2e3032").removeClass("btn-primary").addClass("btn-secondary");
+            $('.card').css("background-color", "#36393f");
+            $('.callout').css("background-color", "#36393f");
+            $('h5, label, p, label, div, button, .badge, a').css("color", "#e2e2e2");
         }
         else {
             document.location.reload(true);
